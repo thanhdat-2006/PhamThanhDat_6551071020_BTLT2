856a03da5f209ae5276f952cf45b436c036071e0
thanhdat-2006 <datpt8593@gmail.com>
2026-09-07 08:31:12 +0700

    Add files via upload

---
+++ TinhLuongNhanVien/NhanVien.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace TinhLuongNhanVien
+{
+    public class NhanVien
+    {
+        private string _maNV;
+        private string _hoTen;
+        private decimal _luongCoBan;
+        private int _soNgayLam;
+        private int _soNgayNghiPhep;
+
+        public string MaNV
+        {
+            get => _maNV;
+            set => _maNV = value;
+        }
+
+        public string HoTen
+        {
+            get => _hoTen;
+            set => _hoTen = value;
+        }
+
+        public decimal LuongCoBan
+        {
+            get => _luongCoBan;
+            set
+            {
+                if (value >= 0)
+                    _luongCoBan = value;
+                else
+                    _luongCoBan = 0;
+            }
+        }
+
+        public int SoNgayLam
+        {
+            get => _soNgayLam;
+            set
+            {
+                if (value >= 0 && value <= 31)
+                    _soNgayLam = value;
+                else
+                    _soNgayLam = 0;
+            }
+        }
+
+        public int SoNgayNghiPhep
+        {
+            get => _soNgayNghiPhep;
+            set => _soNgayNghiPhep = value >= 0 ? value : 0;
+        }
+
+        public decimal LuongThucNhan
+        {
+            get
+            {
+                decimal khauTruBHXH = LuongCoBan * 0.08m;
+                decimal luongTheoNgay = (LuongCoBan / 26m) * SoNgayLam;
+                return luongTheoNgay - khauTruBHXH;
+            }
+        }
+
+        public NhanVien()
+        {
+            _maNV = "ChuaDat";
+            _hoTen = "ChuaDat";
+            _luongCoBan = 0;
+            _soNgayLam = 0;
+            _soNgayNghiPhep = 0;
+        }
+
+        public NhanVien(string maNV, string hoTen, decimal luongCoBan, int soNgayLam, int soNgayNghiPhep)
+        {
+            MaNV = maNV;
+            HoTen = hoTen;
+            LuongCoBan = luongCoBan;
+            SoNgayLam = soNgayLam;
+            SoNgayNghiPhep = soNgayNghiPhep;
+        }
+
+        public NhanVien(string maNV, string hoTen, decimal luong = 5_000_000m, int soNgayLam = 26)
+        {
+            MaNV = maNV;
+            HoTen = hoTen;
+            LuongCoBan = luong;
+            SoNgayLam = soNgayLam;
+            _soNgayNghiPhep = 0;
+        }
+
+        public decimal TinhThuong() => 0m;
+
+        public decimal TinhThuong(decimal heSo) => LuongCoBan * heSo;
+
+        public decimal TinhThuong(decimal heSo, bool coPhucLoi)
+        {
+            decimal thuong = LuongCoBan * heSo;
+            if (coPhucLoi)
+            {
+                thuong += 500_000m;
+            }
+            return thuong;
+        }
+
+        public void InThongTin()
+        {
+            Console.WriteLine($"Mã NV: {MaNV} | Họ Tên: {HoTen} | Lương CB: {LuongCoBan:N0} VNĐ | Ngày công: {SoNgayLam} | Thực nhận: {LuongThucNhan:N0} VNĐ");
+        }
+    }
+}
---
+++ TinhLuongNhanVien/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace TinhLuongNhanVien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("MSSV: 6551071020");
+            Console.WriteLine("BÀI TẬP: TÍNH LƯƠNG NHÂN VIÊN\n");
+
+            NhanVien nv1 = new NhanVien();
+            nv1.MaNV = "NV001";
+            nv1.HoTen = "Nguyễn Văn An";
+            nv1.LuongCoBan = 10_000_000m;
+            nv1.SoNgayLam = 26;
+
+            NhanVien nv2 = new NhanVien(
+                maNV: "NV002",
+                hoTen: "Trần Thị Bình",
+                soNgayLam: 22
+            );
+
+            NhanVien nv3 = new NhanVien("NV003", "Lê Hoàng Cường", 15_000_000m, 25, 1);
+
+            Console.WriteLine("=== DANH SÁCH NHÂN VIÊN ===");
+            nv1.InThongTin();
+            nv2.InThongTin();
+            nv3.InThongTin();
+
+            Console.WriteLine("\n=== SO SÁNH CÁC OVERLOAD TINH THƯỞNG (NV3) ===");
+            decimal heSo = 1.5m;
+
+            decimal thuong1 = nv3.TinhThuong();
+            decimal thuong2 = nv3.TinhThuong(heSo);
+            decimal thuong3 = nv3.TinhThuong(heSo, coPhucLoi: true);
+
+            Console.WriteLine($"1. TinhThuong(): {thuong1,12:N0} VNĐ");
+            Console.WriteLine($"2. TinhThuong({heSo}): {thuong2,12:N0} VNĐ");
+            Console.WriteLine($"3. TinhThuong({heSo}, true): {thuong3,12:N0} VNĐ");
+
+            Console.ReadKey();
+        }
+    }
+}
